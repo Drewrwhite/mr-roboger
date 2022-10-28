2706a35f8c9fb1d77749b685d555bea6cbf5728a
Drew White <d.white0002@gmail.com>
2022-10-28 09:23:42 -0700

    TDD testing to build functionality WIP

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Test: It should return "Beep!" instead of 1 in a string
 Code:
 beepBoop(1)
 Expected Output: "Beep!"
+
+Test: It should return "Boop!" instead of 2 in a string
+Code: 
+beepBoop(2)
+Expected Output: "Boop!"
+
+Test: It should return "Won't you be my neighbor?" instead of 3 in a string
+Code: 
+beepBoop(3)
+Expected Output: "Won't you be my neighbor?"
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 
+//Business Logic
+
 
 function beepBoop(number) {
 
-  const digArray = [];
 
-  for (let i = 0; i <= number; index++) {
-    return i.toString()
-  }
-}
 
-function beepBoop(number) {
+  //   for (let i = 0; i <= number; index++) {
+  //     return i.toString()
+  //   }
+  // }
 
-  for (let i = 0; i <= number; index++) {
-    return (i.toString().includes(1))
-    digArray[i] = "Beep!";
+  function beepBoop(number) {
+    const digArray = [];
+    for (let i = 0; i <= number; index++) {
+      return (i.toString().includes(1))
+      digArray[i] = "Beep!";
+    }
   }
 }
