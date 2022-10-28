82d7f3afd5378d8029529db7e95a4ed8df14cf33
Drew White <d.white0002@gmail.com>
2022-10-28 10:31:08 -0700

    add initial js business logic from results of tdd testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 
 //Business Logic
 
+function beepBoop(number) {
+
+  const digitArray = [];
+
+  for (let i = 0; i <= number; i++) {
+    if(i.toString().includes(3)) {
+      digitArray[i] = "Won't you be my neighbor?";
+    } else if (i.toString().includes(2)) {
+      digitArray[i] = "Boop!";
+    } else if (i.toString().includes(1)) {
+      digitArray[i] = "Beep!";
+    } else {
+      digitArray[i] = i.toString();
+    }
+  }
+
+  let digitOut = "";
+
+  if 
+    (digitArray.forEach(function(element) {
+      digitOut = digitOut + element + " ";
+    })
+  
+  document.getElementById("misterRobogerSong");  
+  
+  return numString.slice(0,digitOut.length - 1);
+
+
+  //User Interface Logic
 
 
 
-
-
-
-
-
-
-
-//User Interface Logic
-
+  event.preventDefault();
