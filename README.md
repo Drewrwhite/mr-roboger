07c49b9faf87dc1ee31677889ab3e120d426c761
Drew White <d.white0002@gmail.com>
2022-10-28 09:08:28 -0700

    Second test WIP

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Describe: beepBoop()
 Test: It should return just "0" as a string when it is inputed
 Code: 
 beepBoop(0)
-Expected Output: 0
+Expected Output: "0"
+
+Test: It should return "Beep!" instead of 1 in a string
+Code:
+beepBoop(1)
+Expected Output: "Beep!"
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 
+
 function beepBoop(number) {
+
+  const digArray = [];
 
   for (let i = 0; i <= number; index++) {
     return i.toString()
+  }
+}
+
+function beepBoop(number) {
+
+  for (let i = 0; i <= number; index++) {
+    return (i.toString().includes(1))
+    digArray[i] = "Beep!";
   }
 }
